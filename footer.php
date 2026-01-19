9895dd1c03f90c85991bd82d15a683a84cc6a344
ibos-io <suraiaafrozmaria@ibos.io>
2026-01-19 18:20:13 +0600

    commit2

--- footer.php
+++ footer.php
@@ -1,1 +1,12 @@
-
+<?php
+// footer.php
+?>
+</main>
+<footer>
+    <div class="container footer-content">
+        <p>9th Floor, Ali Bhaban, 92 Kazi Nazrul Islam Ave, Dhaka 1215, Bangladesh</p>
+        <p>&copy; 2024 Dhaka Consulting. All rights reserved.</p>
+    </div>
+</footer>
+</body>
+</html>
